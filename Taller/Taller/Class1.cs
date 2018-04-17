d1f89ba683bdcf6593436e57cac91d3e235235d8
alulab14 <alulab14@inf.pucp.edu.pe>
2018-04-16 19:29:57 -0500

    Segundo cambio

---
+++ Taller/Taller/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller
+{
+    class Class1
+    {
+        public void funcion()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
--- Taller/Taller/Program.cs
+++ Taller/Taller/Program.cs
@@ -4,19 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace Taller
 {
     static class Program
     {
+        static void funcion2()
+        {
+            Console.WriteLine("World");
+        }
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
-        [STAThread]
+        [STAThread]        
         static void Main()
         {
+            Class1 obj = new Class1();
+            obj.funcion();
+            funcion2();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
         }
     }
 }
